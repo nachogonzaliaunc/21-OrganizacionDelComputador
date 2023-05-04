f177f7e547bf4898fe10eb87b7b397d2bb130829
Nacho Gonzalia <101427060+nachogonzaliaunc@users.noreply.github.com>
2023-05-03 21:47:14 -0300

    Update ej01.s

--- practico 6/ej01.s
+++ practico 6/ej01.s
@@ -6,7 +6,7 @@
     con valores de 1, 2, 3, 4, 5, en base 10, respectivamente.
 */
 
-// I) f = g + h + i + j;
+I) f = g + h + i + j;
 
 ADD X0, X1, X2    // f = 2 + 5 = 5
 ADD X0, X0, X3    // f = 5 + 4 = 9
@@ -14,14 +14,14 @@ ADD X0, X0, X4    // f = 9 + 5 = 14
 
 
 
-// II) f = g + (h + 5);
+II) f = g + (h + 5);
 
 ADDI X0, X2, #5   // f = 3 + 5 = 8 
 ADD X0, X1, X0    // f = 2 + 8 = 10
 
 
 
-// III) f = (g + h) + (g + h);
+III) f = (g + h) + (g + h);
 
 ADD X0, X1, X2    // j = 2 + 3 = 5
 ADD X0, X0, X0    // f = 5 + 5 = 10
